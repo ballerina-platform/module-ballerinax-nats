18570e770deb8bc9a6370a6032e0b10cb26250cb
Arshika Mohottige <arshika.nm@gmail.com>
2020-11-28 21:17:39 +0530

    Update Constants.java

--- nats-native/src/main/java/org/ballerinalang/nats/Constants.java
+++ nats-native/src/main/java/org/ballerinalang/nats/Constants.java
@@ -53,7 +53,7 @@ public class Constants {
 
     public static final String NATS = "nats";
     public static final String ORG_NAME = "ballerinax";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     // Represents nats package.
     public static final String NATS_PACKAGE = ORG_NAME + ORG_NAME_SEPARATOR + NATS + ":" + VERSION;
