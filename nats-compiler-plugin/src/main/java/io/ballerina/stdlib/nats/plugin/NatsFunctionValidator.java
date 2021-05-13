5eecc291b7be99bf1c947ab076811a866cbb442f
Arshika Mohottige <arshika.nm@gmail.com>
2021-05-13 07:09:04 +0530

    Fix compiler plugin test failure

--- nats-compiler-plugin/src/main/java/io/ballerina/stdlib/nats/plugin/NatsFunctionValidator.java
+++ nats-compiler-plugin/src/main/java/io/ballerina/stdlib/nats/plugin/NatsFunctionValidator.java
@@ -19,6 +19,7 @@
 package io.ballerina.stdlib.nats.plugin;
 
 import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.ErrorTypeSymbol;
 import io.ballerina.compiler.api.symbols.MethodSymbol;
 import io.ballerina.compiler.api.symbols.ModuleSymbol;
 import io.ballerina.compiler.api.symbols.ParameterSymbol;
@@ -29,9 +30,11 @@ import io.ballerina.compiler.api.symbols.UnionTypeSymbol;
 import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
 import io.ballerina.compiler.syntax.tree.Node;
 import io.ballerina.compiler.syntax.tree.ParameterNode;
+import io.ballerina.compiler.syntax.tree.QualifiedNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.RequiredParameterNode;
 import io.ballerina.compiler.syntax.tree.SeparatedNodeList;
 import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 import io.ballerina.stdlib.nats.plugin.PluginConstants.CompilationErrors;
 import io.ballerina.tools.diagnostics.DiagnosticSeverity;
@@ -259,20 +262,25 @@ public class NatsFunctionValidator {
     private void validateErrorParam(ParameterNode parameterNode) {
         RequiredParameterNode requiredParameterNode = (RequiredParameterNode) parameterNode;
         Node parameterTypeNode = requiredParameterNode.typeName();
-        SemanticModel semanticModel = context.semanticModel();
-        Optional<Symbol> paramSymbol = semanticModel.symbol(parameterTypeNode);
-        if (paramSymbol.isPresent()) {
-            Optional<ModuleSymbol> moduleSymbol = paramSymbol.get().getModule();
-            if (moduleSymbol.isPresent()) {
-                String paramName = paramSymbol.get().getName().isPresent() ?
-                        paramSymbol.get().getName().get() : "";
-                if (!validateModuleId(moduleSymbol.get()) ||
-                        !paramName.equalsIgnoreCase(PluginConstants.ERROR)) {
-                    context.reportDiagnostic(PluginUtils.getDiagnostic(
-                            CompilationErrors.INVALID_FUNCTION_PARAM_ERROR,
-                            DiagnosticSeverity.ERROR, requiredParameterNode.location()));
+        if (parameterTypeNode.kind() == SyntaxKind.QUALIFIED_NAME_REFERENCE) {
+            QualifiedNameReferenceNode errorNode = (QualifiedNameReferenceNode) parameterTypeNode;
+            SemanticModel semanticModel = context.semanticModel();
+            Optional<Symbol> paramSymbol = semanticModel.symbol(errorNode);
+            if (paramSymbol.isPresent()) {
+                Optional<ModuleSymbol> moduleSymbol = paramSymbol.get().getModule();
+                if (moduleSymbol.isPresent()) {
+                    if (!validateModuleId(moduleSymbol.get()) ||
+                            !(paramSymbol.get() instanceof ErrorTypeSymbol)) {
+                        context.reportDiagnostic(PluginUtils.getDiagnostic(
+                                CompilationErrors.INVALID_FUNCTION_PARAM_ERROR,
+                                DiagnosticSeverity.ERROR, requiredParameterNode.location()));
+                    }
                 }
             }
+        } else {
+            context.reportDiagnostic(PluginUtils.getDiagnostic(
+                    CompilationErrors.INVALID_FUNCTION_PARAM_ERROR,
+                    DiagnosticSeverity.ERROR, requiredParameterNode.location()));
         }
     }
 }
