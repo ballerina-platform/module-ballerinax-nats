4bcccc7e3af3c552e88d127830101a972d8d86d1
aashikam <arshika.nm@gmail.com>
2022-02-15 14:42:26 +0530

    Add log to received messages

--- native/src/main/java/io/ballerina/stdlib/nats/basic/consumer/DefaultMessageHandler.java
+++ native/src/main/java/io/ballerina/stdlib/nats/basic/consumer/DefaultMessageHandler.java
@@ -44,6 +44,8 @@ import io.nats.client.Connection;
 import io.nats.client.Message;
 import io.nats.client.MessageHandler;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
@@ -61,6 +63,7 @@ public class DefaultMessageHandler implements MessageHandler {
     private final Runtime runtime;
     private final NatsMetricsReporter natsMetricsReporter;
     private final Connection natsConnection;
+    private static final PrintStream console;
 
     DefaultMessageHandler(BObject serviceObject, Runtime runtime, Connection natsConnection,
                           NatsMetricsReporter natsMetricsReporter) {
@@ -71,6 +74,10 @@ public class DefaultMessageHandler implements MessageHandler {
         this.natsConnection = natsConnection;
     }
 
+    static {
+        console = System.out;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -79,6 +86,8 @@ public class DefaultMessageHandler implements MessageHandler {
         natsMetricsReporter.reportConsume(message.getSubject(), message.getData().length);
         String replyTo = message.getReplyTo();
         String subject = message.getSubject();
+        String response = new String(message.getData(), StandardCharsets.UTF_8);
+        console.println("Message received java: " + response);
         try {
             if (replyTo != null && getAttachedFunctionType(serviceObject,
                     Constants.ON_REQUEST_RESOURCE) != null) {
